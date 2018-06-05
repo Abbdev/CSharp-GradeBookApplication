c993dc85faa004086fdc7614fa33e540bfb208ee
Abbdev <wangabbo@msu.edu>
2018-06-05 16:33:50 -0400

    adding features

---
+++ GradeBook/GradeBooks/InvalidOperationsException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace GradeBook.GradeBooks
+{
+    [Serializable]
+    internal class InvalidOperationsException : Exception
+    {
+        public InvalidOperationsException()
+        {
+        }
+
+        public InvalidOperationsException(string message) : base(message)
+        {
+        }
+
+        public InvalidOperationsException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected InvalidOperationsException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,4 +1,4 @@
-﻿
+﻿using System;
 using GradeBook.Enums;
 
 namespace GradeBook.GradeBooks
@@ -8,6 +8,17 @@ namespace GradeBook.GradeBooks
         public RankedGradeBook(string name) : base(name)
         {
             Type = GradeBookType.Ranked;
+        }
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+                throw new InvalidOperationsException("Ranked grading requires at least 5 students.");
+            var threshold = (int)Math.Ceiling(Students.Count * 0.2);
+            foreach(var student in Students)
+            {
+
+            }
+            return 'F';
         }
     }
 }
